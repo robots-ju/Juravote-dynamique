947a3fbab3ba26cb0b173a7b8a5db172c8dc0548
GwenaelGirard00 <60039039+GwenaelGirard00@users.noreply.github.com>
2020-07-25 10:04:56 +0200

    images votations 09.02.2020

--- index.php
+++ index.php
@@ -1,1 +1,38 @@
-php
+<!DOCTYPE html>
+<html lang="fr">
+
+<?php $icon = "./IMG/Logo/JuraVote/Icon.png"; ?>
+
+<head>
+    <link rel="shortcut icon" href="<?php echo $icon; ?>"/>
+    <title>JuraVote | Accueil</title>
+
+    <!--meta-->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!--CSS-->
+    <link rel="stylesheet" href="00css/normalize.css">
+    <link rel="stylesheet" href="00css/uikit.min.css"/>
+    <link rel="stylesheet" href="index.css"/>
+    <link rel="stylesheet" href="00css/main.css"/>
+
+    <!--JS-->
+    <script src="00js/uikit.min.js"></script>
+    <script src="00js/uikit-icons.min.js"></script>
+    
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-166072477-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+
+        gtag('js', new Date());
+
+        gtag('config', 'UA-166072477-1');
+    </script>
+
+</head>

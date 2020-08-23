e3be72aeec79751093178a06983ff834ba24b0c0
Daniel Renaud <daniel.renaud2000@bluewin.ch>
2020-08-23 19:59:26 +0200

    Page d'accueil votations et élections

--- Pages/Votations et Elections/accueilVE.php
+++ Pages/Votations et Elections/accueilVE.php
@@ -13,6 +13,7 @@
     <link rel="stylesheet" href="<?php echo $cssUikitMin; ?>"/>
     <link rel="stylesheet" href="<?php echo $cssMain; ?>"/>
     <link rel="stylesheet" href="./index.css"/>
+    <link rel="stylesheet" href="accueilVE.css"/>
     
     <script src="<?php echo $jsUikit ?>"></script>
     <script src="<?php echo $jsUikitIcons ?>"></script>
@@ -45,7 +46,128 @@
     </div>
 </header>
 <main>
+    <section style="padding: 2%;">
+        <h1 class="uk-heading-large">Prochaines votations et élections</h1>
+        <hr> 
+        <!--<div class="switcher-buttons" uk-switcher="animation: uk-animation-fade; toggle: > *">
+                <button class="uk-button uk-button-default" type="button">Prochaines votations</button>
+                <button class="uk-button uk-button-default" type="button">Prochaines élections</button>
+            </div>
+            <ul class="uk-switcher uk-margin">
+                <li>  
 
+                </li>-->
+            <div class="uk-child-width-1-4@m uk-grid-small uk-grid-match" uk-grid>
+                <div>
+                    <div class="uk-card uk-card-default uk-card-hover">
+                        <a href="./federales/federales.html">
+                            <div class="uk-card-body">
+                                <div class="uk-grid-small uk-flex-column" uk-grid>
+                                    <div class="uk-width-auto">
+                                        <img width="150" height="150" src="../../IMG/Logo/Pouvoirs/federal.png"
+                                            alt="drapeau suisse">
+                                    </div>
+                                    <div class="uk-width-expand">
+                                        <h3 class="uk-card-title uk-margin-remove-bottom montserrat">Votations fédérales - 27 septembre 2020</h3>
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+
+                <div>
+                    <div class="uk-card uk-card-default uk-card-hover">
+                        <a href="./communales/communales.html">
+                            <div class="uk-card-body">
+                                <div class="uk-grid-small uk-flex-column" uk-grid>
+                                    <div class="uk-width-auto">
+                                        <img width="150" height="150" src="../../IMG/Logo/Pouvoirs/communal.png" alt="commune">
+                                    </div>
+                                    <div class="uk-width-expand">
+                                        <h3 class="uk-card-title uk-margin-remove-bottom montserrat">Votations communales - 27 septembre 2020</h3>
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+
+                <div>
+                    <div class="uk-card uk-card-default uk-card-hover">
+                        <a href="../04Elections/communales/communales.html">
+                            <div class="uk-card-body">
+                                <div class="uk-grid-small uk-flex-column" uk-grid>
+                                    <div class="uk-width-auto">
+                                        <img width="150" height="150" src="../../IMG/Logo/Pouvoirs/communal.png" alt="commune">
+                                    </div>
+                                    <div class="uk-width-expand">
+                                        <h3 class="uk-card-title uk-margin-remove-bottom montserrat">Elections communales - 27 septembre 2020</h3>
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div>
+                    <div class="uk-card uk-card-default uk-card-hover">
+                        <a href="../04Elections/cantonales/cantonales.html">
+                            <div class="uk-card-body">
+                                <div class="uk-grid-small uk-flex-column" uk-grid>
+                                    <div class="uk-width-auto">
+                                        <img width="150" height="150" src="../../IMG/Logo/Pouvoirs/cantonal.png" alt="drapeau jura">
+                                    </div>
+                                    <div class="uk-width-expand">
+                                        <h3 class="uk-card-title uk-margin-remove-bottom montserrat">Elections cantonales - 18 octobre 2020</h3>
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <!--
+                <div>
+                    <div class="uk-card uk-card-default uk-card-hover">
+                        <a href="./cantonales/cantonales.html">
+                            <div class="uk-card-body">
+                                <div class="uk-grid-small uk-flex-column" uk-grid>
+                                    <div class="uk-width-auto">
+                                        <img width="150" height="150" src="../../IMG/Logo/Pouvoirs/cantonal.png"
+                                            alt="drapeau jura">
+                                    </div>
+                                    <div class="uk-width-expand">
+                                        <h3 class="uk-card-title uk-margin-remove-bottom montserrat">Votations cantonales - à venir</h3>
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="uk-child-width-1-3@m uk-grid-small uk-grid-match" uk-grid>
+                    <div>
+                        <div class="uk-card uk-card-default uk-card-hover">
+                            <a href="../04Elections/federales/federales.html">
+                                <div class="uk-card-body">
+                                    <div class="uk-grid-small uk-flex-column" uk-grid>
+                                        <div class="uk-width-auto">
+                                            <img width="150" height="150" src="../../IMG/Logo/Pouvoirs/federal.png" alt="drapeau suisse">
+                                        </div>
+                                        <div class="uk-width-expand">
+                                            <h3 class="uk-card-title uk-margin-remove-bottom montserrat">Elections fédérales - à venir</h3>
+                                        </div>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>   
+                </div>-->
+            </div>
+            <div class="uk-alert-primary" uk-alert>
+                <a class="uk-alert-close"></a>
+                <p>Les Communes de Courroux et de Beurnevésin n'ont pas souhaité collaborer avec nous. Aucune information
+                    ne sera donc disponible sur les votations de ces communes.</p>
+            </div>                      
+    </section>
 </main>
 </body>
 

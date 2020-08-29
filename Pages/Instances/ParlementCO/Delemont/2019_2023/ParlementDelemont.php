3a99822146f50d9d250a086f1cf2cf1124d6cc38
Daniel Renaud <daniel.renaud2000@bluewin.ch>
2020-08-29 15:13:24 +0200

    Conseil de ville Delémont 2019 - 2023

---
+++ Pages/Instances/ParlementCO/Delemont/2019_2023/ParlementDelemont.php
@@ -0,0 +1,386 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+
+    <?php include '../../../../../variables.php'; ?>
+
+    <link rel="shortcut icon" href="<?php echo $juravoteIcon; ?>"/>
+    <title>JuraVote | Conseil de ville Delémont</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" href="<?php echo $cssNormalize; ?>">
+    <link rel="stylesheet" href="<?php echo $cssUikitMin; ?>"/>
+    <link rel="stylesheet" href="<?php echo $cssMain; ?>"/>
+    <link rel="stylesheet" href="../../../../../index.css"/>
+    <link rel="stylesheet" href="../../../../../badges.css"/>
+    <link rel="stylesheet" href="../../../../instances.css"/>
+    
+    <script src="<?php echo $jsUikit ?>"></script>
+    <script src="<?php echo $jsUikitIcons ?>"></script>
+    <script src="<?php echo $jsUikitMin ?>"></script>
+    <script src="<?php echo $jsUikitIconsMin ?>"></script>
+
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-166072477-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+
+        gtag('js', new Date());
+
+        gtag('config', 'UA-166072477-1');
+    </script>
+</head>
+<body>
+<header>
+    <div class="navbar-normal">
+        <?php include "../../../../../Includes/Navbar/navbar.php"; ?>
+    </div>
+
+    <div>
+        <?php include "../../../../../Includes/Navbar/navbar-mobile.php"; ?>
+    </div>
+</header>
+<main>
+<section style="padding: 2%">
+        <h1 class="uk-heading-large">Conseil de ville de Delémont 2019 - 2023</h1>
+        <hr>
+
+        <div class="uk-child-width-1-5@xl uk-child-width-1-3@l uk-child-width-1-2@m uk-child-width-1-1@s uk-grid-small uk-grid-match" uk-grid>
+            <div>
+                <div class="uk-card uk-card-default uk-text-center">
+                    <div class="uk-card-badge uk-label badge-ps uk-border-rounded">PS</div><br>
+                    <div class="uk-card-body">
+                        <h3 class="uk-card-title-center text-rouge-fonce">Ali Iskander</h3>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div class="uk-card uk-card-default uk-text-center">
+                    <div class="uk-card-badge uk-label badge-ps uk-border-rounded">PS</div><br>
+                    <div class="uk-card-body">
+                        <h3 class="uk-card-title-center text-rouge-fonce">Christophe Badertscher</h3>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div class="uk-card uk-card-default uk-text-center">
+                    <div class="uk-card-badge uk-label badge-ps uk-border-rounded">PS</div><br>
+                    <div class="uk-card-body">
+                        <h3 class="uk-card-title-center text-rouge-fonce">Pierre Brulhart</h3>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div class="uk-card uk-card-default uk-text-center">
+                    <div class="uk-card-badge uk-label badge-ps uk-border-rounded">PS</div><br>
+                    <div class="uk-card-body">
+                        <h3 class="uk-card-title-center text-rouge-fonce">Laurent Crevoisier</h3>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div class="uk-card uk-card-default uk-text-center">
+                    <div class="uk-card-badge uk-label badge-ps uk-border-rounded">PS</div><br>
+                    <div class="uk-card-body">
+                        <h3 class="uk-card-title-center text-rouge-fonce">Carole Frossard</h3>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div class="uk-card uk-card-default uk-text-center">
+                    <div class="uk-card-badge uk-label badge-ps uk-border-rounded">PS</div><br>
+                    <div class="uk-card-body">
+                        <h3 class="uk-card-title-center text-rouge-fonce">Gaëlle Frossard</h3>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div class="uk-card uk-card-default uk-text-center">
+                    <div class="uk-card-badge uk-label badge-ps uk-border-rounded">PS</div><br>
+                    <div class="uk-card-body">
+                        <h3 class="uk-card-title-center text-rouge-fonce">Besim Hoxha</h3>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div class="uk-card uk-card-default uk-text-center">
+                    <div class="uk-card-badge uk-label badge-ps uk-border-rounded">PS</div><br>
+                    <div class="uk-card-body">
+                        <h3 class="uk-card-title-center text-rouge-fonce">Thierry Kamber</h3>
+                    </div>
+                </div>
+            </div> 
+            <div>
+                <div class="uk-card uk-card-default uk-text-center">
+                    <div class="uk-card-badge uk-label badge-ps uk-border-rounded">PS</div><br>
+                    <div class="uk-card-body">
+                        <h3 class="uk-card-title-center text-rouge-fonce">Agnès Maeder</h3>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div class="uk-card uk-card-default uk-text-center">
+                    <div class="uk-card-badge uk-label badge-ps uk-border-rounded">PS</div><br>
+                    <div class="uk-card-body">
+                        <h3 class="uk-card-title-center text-rouge-fonce">Grégoire Monin</h3>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div class="uk-card uk-card-default uk-text-center">
+                    <div class="uk-card-badge uk-label badge-ps uk-border-rounded">PS</div><br>
+                    <div class="uk-card-body">
+                        <h3 class="uk-card-title-center text-rouge-fonce">Marc Ribeaud</h3>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div class="uk-card uk-card-default uk-text-center">
+                    <div class="uk-card-badge uk-label badge-ps uk-border-rounded">PS</div><br>
+                    <div class="uk-card-body">
+                        <h3 class="uk-card-title-center text-rouge-fonce">Jude Schindeholz</h3>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div class="uk-card uk-card-default uk-text-center">
+                    <div class="uk-card-badge uk-label badge-ps uk-border-rounded">PS</div><br>
+                    <div class="uk-card-body">
+                        <h3 class="uk-card-title-center text-rouge-fonce">Annie Schneider</h3>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div class="uk-card uk-card-default uk-text-center">
+                    <div class="uk-card-badge uk-label badge-ps uk-border-rounded">PS</div><br>
+                    <div class="uk-card-body">
+                        <h3 class="uk-card-title-center text-rouge-fonce">Mehmet Suvat</h3>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div class="uk-card uk-card-default uk-text-center">
+                    <div class="uk-card-badge uk-label badge-gauche uk-border-rounded">Alternative de gauche</div><br>
+                    <div class="uk-card-body">
+                        <h3 class="uk-card-title-center text-rouge-fonce">Théo Burri</h3>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div class="uk-card uk-card-default uk-text-center">
+                    <div class="uk-card-badge uk-label badge-gauche uk-border-rounded">Alternative de gauche</div><br>
+                    <div class="uk-card-body">
+                        <h3 class="uk-card-title-center text-rouge-fonce">Jérôme Corbat</h3>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div class="uk-card uk-card-default uk-text-center">
+                    <div class="uk-card-badge uk-label badge-gauche uk-border-rounded">Alternative de gauche</div><br>
+                    <div class="uk-card-body">
+                        <h3 class="uk-card-title-center text-rouge-fonce">Rémy Meury</h3>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div class="uk-card uk-card-default uk-text-center">
+                    <div class="uk-card-badge uk-label badge-gauche uk-border-rounded">Alternative de gauche</div><br>
+                    <div class="uk-card-body">
+                        <h3 class="uk-card-title-center text-rouge-fonce">Célien Milani</h3>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div class="uk-card uk-card-default uk-text-center">
+                    <div class="uk-card-badge uk-label badge-gauche uk-border-rounded">Alternative de gauche</div><br>
+                    <div class="uk-card-body">
+                        <h3 class="uk-card-title-center text-rouge-fonce">Céline Robert-Charrue</h3>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div class="uk-card uk-card-default uk-text-center">
+                    <div class="uk-card-badge uk-label badge-gauche uk-border-rounded">Alternative de gauche</div><br>
+                    <div class="uk-card-body">
+                        <h3 class="uk-card-title-center text-rouge-fonce">Magali Rohner</h3>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div class="uk-card uk-card-default uk-text-center">
+                    <div class="uk-card-badge uk-label badge-gauche uk-border-rounded">Alternative de gauche</div><br>
+                    <div class="uk-card-body">
+                        <h3 class="uk-card-title-center text-rouge-fonce">Gabriel Villemin</h3>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div class="uk-card uk-card-default uk-text-center">
+                    <div class="uk-card-badge uk-label badge-pdc uk-border-rounded">PDC</div><br>
+                    <div class="uk-card-body">
+                        <h3 class="uk-card-title-center text-rouge-fonce">Camille Borruat</h3>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div class="uk-card uk-card-default uk-text-center">
+                    <div class="uk-card-badge uk-label badge-pdc uk-border-rounded">PDC</div><br>
+                    <div class="uk-card-body">
+                        <h3 class="uk-card-title-center text-rouge-fonce">Patrick Claude</h3>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div class="uk-card uk-card-default uk-text-center">
+                    <div class="uk-card-badge uk-label badge-pdc uk-border-rounded">PDC</div><br>
+                    <div class="uk-card-body">
+                        <h3 class="uk-card-title-center text-rouge-fonce">Gaëtan Farron</h3>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div class="uk-card uk-card-default uk-text-center">
+                    <div class="uk-card-badge uk-label badge-pdc uk-border-rounded">PDC</div><br>
+                    <div class="uk-card-body">
+                        <h3 class="uk-card-title-center text-rouge-fonce">Pierre-Alain Fleury</h3>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div class="uk-card uk-card-default uk-text-center">
+                    <div class="uk-card-badge uk-label badge-pdc uk-border-rounded">PDC</div><br>
+                    <div class="uk-card-body">
+                        <h3 class="uk-card-title-center text-rouge-fonce">Patrick Frein</h3>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div class="uk-card uk-card-default uk-text-center">
+                    <div class="uk-card-badge uk-label badge-pdc uk-border-rounded">PDC</div><br>
+                    <div class="uk-card-body">
+                        <h3 class="uk-card-title-center text-rouge-fonce">Khelaf Kerkour</h3>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div class="uk-card uk-card-default uk-text-center">
+                    <div class="uk-card-badge uk-label badge-pdc uk-border-rounded">PDC</div><br>
+                    <div class="uk-card-body">
+                        <h3 class="uk-card-title-center text-rouge-fonce">Olivier Montavon</h3>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div class="uk-card uk-card-default uk-text-center">
+                    <div class="uk-card-badge uk-label badge-pdc uk-border-rounded">PDC</div><br>
+                    <div class="uk-card-body">
+                        <h3 class="uk-card-title-center text-rouge-fonce">Claude Noirjean</h3>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div class="uk-card uk-card-default uk-text-center">
+                    <div class="uk-card-badge uk-label badge-pcsi uk-border-rounded">PCSI</div><br>
+                    <div class="uk-card-body">
+                        <h3 class="uk-card-title-center text-rouge-fonce">Marie Blandino</h3>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div class="uk-card uk-card-default uk-text-center">
+                    <div class="uk-card-badge uk-label badge-pcsi uk-border-rounded">PCSI</div><br>
+                    <div class="uk-card-body">
+                        <h3 class="uk-card-title-center text-rouge-fonce">Sophie Chevrey-Schaller</h3>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div class="uk-card uk-card-default uk-text-center">
+                    <div class="uk-card-badge uk-label badge-pcsi uk-border-rounded">PCSI</div><br>
+                    <div class="uk-card-body">
+                        <h3 class="uk-card-title-center text-rouge-fonce">Florine Jardin</h3>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div class="uk-card uk-card-default uk-text-center">
+                    <div class="uk-card-badge uk-label badge-pcsi uk-border-rounded">PCSI</div><br>
+                    <div class="uk-card-body">
+                        <h3 class="uk-card-title-center text-rouge-fonce">Alexandre Kaiser</h3>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div class="uk-card uk-card-default uk-text-center">
+                    <div class="uk-card-badge uk-label badge-pcsi uk-border-rounded">PCSI</div><br>
+                    <div class="uk-card-body">
+                        <h3 class="uk-card-title-center text-rouge-fonce">Suzanne Maître</h3>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div class="uk-card uk-card-default uk-text-center">
+                    <div class="uk-card-badge uk-label badge-pcsi uk-border-rounded">PCSI</div><br>
+                    <div class="uk-card-body">
+                        <h3 class="uk-card-title-center text-rouge-fonce">Thérèse Schindelholz</h3>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div class="uk-card uk-card-default uk-text-center">
+                    <div class="uk-card-badge uk-label badge-plr uk-border-rounded">PLR</div><br>
+                    <div class="uk-card-body">
+                        <h3 class="uk-card-title-center text-rouge-fonce">Pierre Chételat</h3>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div class="uk-card uk-card-default uk-text-center">
+                    <div class="uk-card-badge uk-label badge-plr uk-border-rounded">PLR</div><br>
+                    <div class="uk-card-body">
+                        <h3 class="uk-card-title-center text-rouge-fonce">Pascal Domont</h3>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div class="uk-card uk-card-default uk-text-center">
+                    <div class="uk-card-badge uk-label badge-plr uk-border-rounded">PLR</div><br>
+                    <div class="uk-card-body">
+                        <h3 class="uk-card-title-center text-rouge-fonce">Michel Friche</h3>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div class="uk-card uk-card-default uk-text-center">
+                    <div class="uk-card-badge uk-label badge-plr uk-border-rounded">PLR</div><br>
+                    <div class="uk-card-body">
+                        <h3 class="uk-card-title-center text-rouge-fonce">Christophe Günter</h3>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div class="uk-card uk-card-default uk-text-center">
+                    <div class="uk-card-badge uk-label badge-udc uk-border-rounded">UDC</div><br>
+                    <div class="uk-card-body">
+                        <h3 class="uk-card-title-center text-rouge-fonce">Laurence Studer</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</main>
+</body>
+
+
+<footer>
+    <div class="footer">
+    <?php include $_SERVER['DOCUMENT_ROOT']."/Juravote-dynamique/Includes/Footer/footer.php"; ?>
+    </div>
+</footer>
+</html>
---
+++ Pages/Instances/ParlementCO/ParlementCO.php
@@ -0,0 +1,155 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+
+    <?php include '../../../variables.php'; ?>
+
+    <link rel="shortcut icon" href="<?php echo $juravoteIcon; ?>"/>
+    <title>JuraVote | Législatifs communaux Jura</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" href="<?php echo $cssNormalize; ?>">
+    <link rel="stylesheet" href="<?php echo $cssUikitMin; ?>"/>
+    <link rel="stylesheet" href="<?php echo $cssMain; ?>"/>
+    <link rel="stylesheet" href="../../../index.css"/>
+    <link rel="stylesheet" href="../../../badges.css"/>
+    <link rel="stylesheet" href="../../instances.css"/>
+    
+    <script src="<?php echo $jsUikit ?>"></script>
+    <script src="<?php echo $jsUikitIcons ?>"></script>
+    <script src="<?php echo $jsUikitMin ?>"></script>
+    <script src="<?php echo $jsUikitIconsMin ?>"></script>
+
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-166072477-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+
+        gtag('js', new Date());
+
+        gtag('config', 'UA-166072477-1');
+    </script>
+</head>
+<body>
+<header>
+    <div class="navbar-normal">
+        <?php include "../../../Includes/Navbar/navbar.php"; ?>
+    </div>
+
+    <div>
+    <?php include "../../../Includes/Navbar/navbar-mobile.php"; ?>
+    </div>
+</header>
+<main>
+    <section style="padding: 2%">
+        <h1 class="uk-heading-large">Législatifs communaux (Jura)</h1>
+        <hr>
+        <div class="uk-child-width-1-5@m uk-grid-small uk-grid-match" uk-grid>
+            <div>
+                <div class="uk-card uk-card-default uk-card-hover">
+                    <a href="Delemont/2019_2023/ParlementDelemont.php">
+                        <div class="uk-card-header">
+                            <div class="uk-width-auto">
+                                <img class="illustration" src="<?php echo $communeDelemont ?>" alt="Commune de Delémont">
+                            </div>
+                        </div>
+                        <div class="uk-card-body">
+                            <div class="uk-grid-small uk-flex-column" uk-grid>
+
+                                <div class="uk-width-expand">
+                                    <h3 class="uk-card-title uk-margin-remove-bottom montserrat">Delémont</h3>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+
+            <div>
+                <div class="uk-card uk-card-default uk-card-hover">
+                    <a href="HauteSorne/2019_2023/ParlementHauteSorne.php">
+                        <div class="uk-card-header">
+                            <div class="uk-width-auto">
+                                <img class="illustration" src="<?php echo $communeHauteSorne ?>" alt="Commune de Haute-Sorne">
+                            </div>
+                        </div>
+                        <div class="uk-card-body">
+                            <div class="uk-width-expand">
+                                <h3 class="uk-card-title uk-margin-remove-bottom montserrat">Haute-Sorne</h3>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+
+            <div>
+                <div class="uk-card uk-card-default uk-card-hover">
+                    <a href="ValTerbi/2019_2023/ParlementValTerbi.php">
+                        <div class="uk-card-header">
+                            <div class="uk-width-auto">
+                                <img class="illustration" src="<?php echo $communeValTerbi ?>" alt="Commune de Val-Terbi">
+                            </div>
+                        </div>
+                        <div class="uk-card-body">
+                            <div class="uk-width-expand">
+                                <h3 class="uk-card-title uk-margin-remove-bottom montserrat">Val-Terbi</h3>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div>
+                <div class="uk-card uk-card-default uk-card-hover">
+                    <a href="Porrentruy/2019_2023/ParlementPorrentruy.php">
+                        <div class="uk-card-header">
+                            <div class="uk-width-auto">
+                                <img class="illustration" src="<?php echo $communePorrentruy ?>" alt="Commune de Porrentruy">
+                            </div>
+                        </div>
+                        <div class="uk-card-body">
+                            <div class="uk-width-expand">
+                                <h3 class="uk-card-title uk-margin-remove-bottom montserrat">Porrentruy</h3>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div>
+                <div class="uk-card uk-card-default uk-card-hover">
+                    <a href="LesBois/2019_2023/ParlementLesBois.php">
+                        <div class="uk-card-header">
+                            <div class="uk-width-auto">
+                                <img class="illustration" src="<?php echo $communeLesBois ?>" alt="Commune des Bois">
+                            </div>
+                        </div>
+                        <div class="uk-card-body">
+                            <div class="uk-width-expand">
+                                <h3 class="uk-card-title uk-margin-remove-bottom montserrat">Les Bois</h3>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <h3 class="uk-text-left uk-margin-auto" style="padding-top: 2%;">Les autres communes ne disposent pas de Conseils de ville ou de Conseils
+            généraux. En effet, dans les communes plus petites, ce sont des Assemblées communales qui exercent le pouvoir
+            législatif, donc chaque citoyen qui peut exercer ses droits civils ose prendre part à cette Assemblée
+            communale.</h3>
+    </section>
+
+</main>
+</body>
+
+
+<footer>
+    <div class="footer">
+    <?php include $_SERVER['DOCUMENT_ROOT']."/Juravote-dynamique/Includes/Footer/footer.php"; ?>
+    </div>
+</footer>
+</html>

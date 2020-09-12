a3b6c77311b062d921f57094aef084e6ddc2a82a
Daniel Renaud <daniel.renaud2000@bluewin.ch>
2020-09-12 21:30:20 +0200

    MAJ votations 27.09.2020 #1

--- Pages/Votations et Elections/Votations/VotationsCH/AvionsCombat.php
+++ Pages/Votations et Elections/Votations/VotationsCH/AvionsCombat.php
@@ -73,117 +73,58 @@
                 </li>
                 <li>
                     <h2>Partis fédéraux</h2>
-                    <div id="chartdivCH"></div>
-                    <script>
-                        am4core.useTheme(am4themes_animated);
-
-                        var chartCH = am4core.create("chartdivCH", am4charts.XYChart);
-                        chartCH.hiddenState.properties.opacity = 0; // this creates initial fade-in
-
-                        chartCH.paddingBottom = 30;
-
-                        chartCH.data = [{
-                            "name": "Verts",
-                            "steps": 10
-                        }, {
-                            "name": "PS",
-                            "steps": 10
-                        }, {
-                            "name": "PEV",
-                            "steps": 40
-                        }, {
-                            "name": "PVL",
-                            "steps": 40
-                        }, {
-                            "name": "PDC",
-                            "steps": 40
-                        }, {
-                            "name": "PBD",
-                            "steps": 40
-                        }, {
-                            "name": "PLR",
-                            "steps": 40
-                        }, {
-                            "name": "UDC",
-                            "steps": 40
-                        }];
-
-                        var categoryAxis = chartCH.xAxes.push(new am4charts.CategoryAxis());
-                        categoryAxis.dataFields.category = "name";
-                        categoryAxis.renderer.grid.template.strokeOpacity = 0;
-                        categoryAxis.renderer.minGridDistance = 10;
-                        categoryAxis.renderer.labels.template.dy = 35;
-                        categoryAxis.renderer.tooltip.dy = 35;
-
-                        var valueAxis = chartCH.yAxes.push(new am4charts.ValueAxis());
-                        valueAxis.renderer.inside = true;
-                        valueAxis.renderer.labels.template.fillOpacity = 0.3;
-                        valueAxis.renderer.grid.template.strokeOpacity = 0;
-                        valueAxis.min = 0;
-                        valueAxis.cursorTooltipEnabled = false;
-                        valueAxis.renderer.baseGrid.strokeOpacity = 0;
-
-                        var series = chartCH.series.push(new am4charts.ColumnSeries);
-                        series.dataFields.valueY = "steps";
-                        series.dataFields.categoryX = "name";
-                        series.tooltipText = "{valueY.value}";
-                        series.tooltip.pointerOrientation = "vertical";
-                        series.tooltip.dy = - 6;
-                        series.columnsContainer.zIndex = 100;
-
-                        var columnTemplate = series.columns.template;
-                        columnTemplate.width = am4core.percent(50);
-                        columnTemplate.maxWidth = 66;
-                        columnTemplate.column.cornerRadius(60, 60, 10, 10);
-                        columnTemplate.strokeOpacity = 0;
-
-                        series.heatRules.push({ target: columnTemplate, property: "fill", dataField: "valueY", min: am4core.color("#a00000"), max: am4core.color("#35ff00") });
-                        series.mainContainer.mask = undefined;
-
-                        var bullet = columnTemplate.createChild(am4charts.CircleBullet);
-                        bullet.circle.radius = 30;
-                        bullet.valign = "bottom";
-                        bullet.align = "center";
-                        bullet.isMeasured = true;
-                        bullet.mouseEnabled = false;
-                        bullet.verticalCenter = "bottom";
-
-                        var hoverState = bullet.states.create("hover");
-                        var outlineCircle = bullet.createChild(am4core.Circle);
-                        outlineCircle.adapter.add("radius", function (radius, target) {
-                            var circleBullet = target.parent;
-                            return circleBullet.circle.pixelRadius + 10;
-                        })                    
-
-                        image.adapter.add("href", function (href, target) {
-                            var dataItem = target.dataItem;
-                            if (dataItem) {
-                                return "https://s3-us-west-2.amazonaws.com/s.cdpn.io/t-160/" + dataItem.categoryX.toLowerCase() + ".jpg";
-                            }
-                        })                    
-
-                        var previousBullet;
-                        chartCH.cursor.events.on("cursorpositionchanged", function (event) {
-                            var dataItem = series.tooltipDataItem;
-
-                            if (dataItem.column) {
-                                var bullet = dataItem.column.children.getIndex(1);
-
-                                if (previousBullet && previousBullet != bullet) {
-                                    previousBullet.isHover = false;
-                                }
-
-                                if (previousBullet != bullet) {
-
-                                    var hs = bullet.states.getKey("hover");
-                                    hs.properties.dy = -bullet.parent.pixelHeight + 30;
-                                    bullet.isHover = true;
-
-                                    previousBullet = bullet;
-                                }
-                            }
-                        })
-                    </script>
+                    <div>
+                        <div class="uk-child-width-1-4@m uk-grid-small uk-grid-match" uk-grid>
+                            <div>
+                                <div class="uk-card uk-card-default uk-card-body">
+                                    <h3 class="uk-card-title uk-text-center"><span uk-icon="icon: minus-circle; ratio: 2.5"></span></h3>
+                                    <p class="uk-text-center">Verts</p>
+                                </div>
+                            </div>
+                            <div>
+                                <div class="uk-card uk-card-default uk-card-body">
+                                    <h3 class="uk-card-title uk-text-center"><span uk-icon="icon: minus-circle; ratio: 2.5"></span></h3>
+                                    <p class="uk-text-center">PS</p>
+                                </div>
+                            </div>
+                            <div>
+                                <div class="uk-card uk-card-default uk-card-body">
+                                    <h3 class="uk-card-title uk-text-center"><span uk-icon="plus-circle; ratio: 2.5"></span></h3>
+                                    <p class="uk-text-center">PEV</p>
+                                </div>
+                            </div>
+                            <div>
+                                <div class="uk-card uk-card-default uk-card-body">
+                                    <h3 class="uk-card-title uk-text-center"><span uk-icon="icon:  plus-circle; ratio: 2.5"></span></h3>
+                                    <p class="uk-text-center">PVL</p>
+                                </div>
+                            </div>
+                            <div>
+                                <div class="uk-card uk-card-default uk-card-body">
+                                    <h3 class="uk-card-title uk-text-center"><span uk-icon="icon: plus-circle; ratio: 2.5"></span></h3>
+                                    <p class="uk-text-center">PDC</p>
+                                </div>
+                            </div>
+                            <div>
+                                <div class="uk-card uk-card-default uk-card-body">
+                                    <h3 class="uk-card-title uk-text-center"><span uk-icon="icon: plus-circle; ratio: 2.5"></span></h3>
+                                    <p class="uk-text-center">PBD</p>
+                                </div>
+                            </div>
+                            <div>
+                                <div class="uk-card uk-card-default uk-card-body">
+                                    <h3 class="uk-card-title uk-text-center"><span uk-icon="icon: plus-circle; ratio: 2.5"></span></h3>
+                                    <p class="uk-text-center">PLR</p>
+                                </div>
+                            </div>
+                            <div>
+                                <div class="uk-card uk-card-default uk-card-body">
+                                    <h3 class="uk-card-title uk-text-center"><span uk-icon="icon: plus-circle; ratio: 2.5"></span></h3>
+                                    <p class="uk-text-center">UDC</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
 
                     <h2>Partis jurassiens</h2>
                     <div id="chartdivJU"></div>

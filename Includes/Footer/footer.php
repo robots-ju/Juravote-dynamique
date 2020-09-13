cf1aad74dbc627ba1b2bd6fd614cad9c75af85e3
Daniel Renaud <daniel.renaud2000@bluewin.ch>
2020-09-13 13:17:15 +0200

    Revert "Revert "WALA""
    This reverts commit 50116505e20ec27d95e4c778b0130c42d8e8381b.

--- Includes/Footer/footer.php
+++ Includes/Footer/footer.php
@@ -5,11 +5,6 @@
         <base target="_parent">
     </head>
     <body>
-
-    <div class="separateur">
-
-    </div>
-
         <footer class="footer"> 
             <div class="uk-grid-collapse uk-child-width-1-2@l uk-text-left uk-flex uk-grid-match" uk-grid>
                 <div class="nav-rapide" style="padding: 2%;">

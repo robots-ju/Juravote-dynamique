0934d1e6b5d7c006595591ae87dbfd4025277bac
Daniel Renaud <daniel.renaud2000@bluewin.ch>
2020-09-27 20:50:03 +0200

    suppression /juravote-dynamqiue

--- Pages/Resultats/VotationsCO/Delemont/2020-09-27/ComplexeScolaire.php
+++ Pages/Resultats/VotationsCO/Delemont/2020-09-27/ComplexeScolaire.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-        <?php include $_SERVER['DOCUMENT_ROOT'].'/Juravote-dynamique/variables.php'; ?>
+        <?php include $_SERVER['DOCUMENT_ROOT'].'/variables.php'; ?>
 
         <link rel="shortcut icon" href="<?php echo $juravoteIcon; ?>"/>
         <title>JuraVote | Votation Delémont 27.09.2020</title>
@@ -41,7 +41,7 @@
 <!-- Barre de navigation -->
 <header>
             <div class="navbar-normal">
-                <?php include $_SERVER['DOCUMENT_ROOT']."/Juravote-dynamique/Includes/Navbar/navbar.php"; ?>
+                <?php include $_SERVER['DOCUMENT_ROOT']."/Includes/Navbar/navbar.php"; ?>
             </div>
 
             <div>

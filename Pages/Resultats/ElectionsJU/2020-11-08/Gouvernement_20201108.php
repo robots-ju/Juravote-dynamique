e0b75434c41691f71b342449f25fdafaf4004fe7
Daniel Renaud <daniel.renaud2000@bluewin.ch>
2020-11-08 13:23:07 +0100

    Update Gouvernement_20201108.php

--- Pages/Resultats/ElectionsJU/2020-11-08/Gouvernement_20201108.php
+++ Pages/Resultats/ElectionsJU/2020-11-08/Gouvernement_20201108.php
@@ -261,14 +261,14 @@
                                 </div>
                             </div>-->
 
-                            <!--<a class="uk-button uk-button-link" href="#modal-container22" uk-toggle>Basse-Allaine</a>
+                            <a class="uk-button uk-button-link" href="#modal-container22" uk-toggle>Basse-Allaine</a>
                             <div id="modal-container22" class="uk-modal-container" uk-modal>
                                 <div class="uk-modal-dialog uk-modal-body">
                                     <button class="uk-modal-close-default" type="button" uk-close></button>
                                     <iframe title="Basse-Allaine - élection du Gouvernement jurassien (2e tour)" aria-label="Histogramme" id="datawrapper-chart-O9eYW" src="https://datawrapper.dwcdn.net/O9eYW/1/" scrolling="no" frameborder="0" style="width: 0; min-width: 100% !important; border: none;" height="261"></iframe><script type="text/javascript">!function(){"use strict";window.addEventListener("message",(function(a){if(void 0!==a.data["datawrapper-height"])for(var e in a.data["datawrapper-height"]){var t=document.getElementById("datawrapper-chart-"+e)||document.querySelector("iframe[src*='"+e+"']");t&&(t.style.height=a.data["datawrapper-height"][e]+"px")}}))}();
                                     </script>
                                 </div>
-                            </div>-->
+                            </div>
 
                             <a class="uk-button uk-button-link" href="#modal-container23" uk-toggle>Beurnevésin</a>
                             <div id="modal-container23" class="uk-modal-container" uk-modal>

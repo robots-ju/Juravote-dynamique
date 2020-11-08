3effebaee5a775bac3eea086aaceaf8a84b973e7
Daniel Renaud <daniel.renaud2000@bluewin.ch>
2020-11-08 14:18:02 +0100

    Update Gouvernement_20201108.php

--- Pages/Resultats/ElectionsJU/2020-11-08/Gouvernement_20201108.php
+++ Pages/Resultats/ElectionsJU/2020-11-08/Gouvernement_20201108.php
@@ -414,14 +414,14 @@
                                 </div>
                             </div>
 
-                            <!--<a class="uk-button uk-button-link" href="#modal-container38" uk-toggle>Porrentruy</a>
+                            <a class="uk-button uk-button-link" href="#modal-container38" uk-toggle>Porrentruy</a>
                             <div id="modal-container38" class="uk-modal-container" uk-modal>
                                 <div class="uk-modal-dialog uk-modal-body">
                                     <button class="uk-modal-close-default" type="button" uk-close></button>
                                     <iframe title="Porrentruy - élection du Gouvernement jurassien (2e tour)" aria-label="Histogramme" id="datawrapper-chart-eGxx6" src="https://datawrapper.dwcdn.net/eGxx6/1/" scrolling="no" frameborder="0" style="width: 0; min-width: 100% !important; border: none;" height="261"></iframe><script type="text/javascript">!function(){"use strict";window.addEventListener("message",(function(a){if(void 0!==a.data["datawrapper-height"])for(var e in a.data["datawrapper-height"]){var t=document.getElementById("datawrapper-chart-"+e)||document.querySelector("iframe[src*='"+e+"']");t&&(t.style.height=a.data["datawrapper-height"][e]+"px")}}))}();
                                     </script>
                                 </div>
-                            </div>-->
+                            </div>
 
                             <a class="uk-button uk-button-link" href="#modal-container39" uk-toggle>Vendlincourt</a>
                             <div id="modal-container39" class="uk-modal-container" uk-modal>

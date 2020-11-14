6348798cdc73211b188bcc9ab8cae37933ca51c8
Daniel Renaud <daniel.renaud2000@bluewin.ch>
2020-11-14 15:57:10 +0100

    /Juravote-dynamique

--- Pages/Instances/CommissionsCO/Val Terbi/2019_2023/GouvernementVal Terbi.php
+++ Pages/Instances/CommissionsCO/Val Terbi/2019_2023/GouvernementVal Terbi.php
@@ -39,11 +39,11 @@
 <body>
 <header>
             <div class="navbar-normal">
-                <?php include $_SERVER['DOCUMENT_ROOT']."/Juravote-dynamique/Includes/Navbar/navbar.php"; ?>
+                <?php include $_SERVER['DOCUMENT_ROOT']."/Includes/Navbar/navbar.php"; ?>
             </div>
 
             <div>
-                <?php include $_SERVER['DOCUMENT_ROOT']."/Juravote-dynamique/Includes/Navbar/navbar-mobile.php"; ?>
+                <?php include $_SERVER['DOCUMENT_ROOT']."/Includes/Navbar/navbar-mobile.php"; ?>
             </div>
         </header>
 <main>
@@ -110,7 +110,7 @@
 </main>
 <footer>
         <div class="footer">
-            <?php include $_SERVER['DOCUMENT_ROOT']."/Juravote-dynamique/Includes/Footer/footer.php"; ?>
+            <?php include $_SERVER['DOCUMENT_ROOT']."/Includes/Footer/footer.php"; ?>
         </div>
     </footer>
 </body>

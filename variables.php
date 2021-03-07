942054030fdabf36050bb6e1e618881336d1ae9c
Daniel Renaud <daniel.renaud2000@bluewin.ch>
2021-03-07 14:38:43 +0100

    Update variables.php

--- variables.php
+++ variables.php
@@ -109,7 +109,7 @@
             $PrudatBrice = "/IMG/Personnalites/ElectionsJU/ParlementA/VERTS_PrudatBrice.png";
 
         //Election complémentaire à la Mairie du Noirmont du 21 mars 2021
-        $GabrielMartinoli = "/Juravote-dynamique/IMG/Elections/LeNoirmont_20210321/MartinoliGabriel2.jpeg";
+        $GabrielMartinoli = "/IMG/Elections/LeNoirmont_20210321/MartinoliGabriel2.jpeg";
         $DamienParatte = "/Juravote-dynamique/IMG/Elections/LeNoirmont_20210321/ParatteDamien.webp";
         
     //juravote :

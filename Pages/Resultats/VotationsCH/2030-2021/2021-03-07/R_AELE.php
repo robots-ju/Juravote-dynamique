af20a0955db12b60289ad639018d4ead1ccadd2c
Daniel Renaud <daniel.renaud2000@bluewin.ch>
2021-03-07 16:05:58 +0100

    participaton 7.3.21

--- Pages/Resultats/VotationsCH/2030-2021/2021-03-07/R_AELE.php
+++ Pages/Resultats/VotationsCH/2030-2021/2021-03-07/R_AELE.php
@@ -65,7 +65,7 @@
                     <div class="uk-card-body uk-text-left">
                         <p><b>Objet : </b>Accord de partenariat économique avec l’Indonésie</p>
                         <p><b>Participation : </b>45.00 %</p>
-                        <p><b>Résultats : </b>53.60 % de OUI</p>
+                        <p><b>Résultats : </b>52.30 % de OUI</p>
                         <p><b>Résultat : </b>Accepté (provisoire)</p>
                         <p><b>Type de la votation : </b>Votation fédérale</p>
                         <p><b>Mode de votation : </b>Référendum facultatif</p>
--- Pages/Resultats/VotationsCH/2030-2021/2021-03-07/R_Voile.php
+++ Pages/Resultats/VotationsCH/2030-2021/2021-03-07/R_Voile.php
@@ -65,8 +65,8 @@
                     <div class="uk-card-body uk-text-left">
                         <p><b>Objet : </b>Initiative populaire "Oui à l'interdiction de se dissimuler le visage"</p>
                         <p><b>Participation : </b>45.00 %</p>
-                        <p><b>Résultats du peuple: </b>53.30 % de OUI</p>
-                        <p><b>Résultats des cantons : </b>15.5 / 23 de OUI</p>
+                        <p><b>Résultats du peuple: </b>52.70 % de OUI</p>
+                        <p><b>Résultats des cantons : </b>17 / 23 de OUI</p>
                         <p><b>Résultat final : </b>Accepté (provisoire)</p>
                         <p><b>Type de la votation : </b>Votation fédérale</p>
                         <p><b>Mode de votation : </b>Initiative populaire</p>

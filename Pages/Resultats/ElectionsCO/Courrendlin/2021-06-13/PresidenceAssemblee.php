3454ed23e9320d6a65d646fe86fa45b42c871cc6
Daniel Renaud <daniel.renaud2000@bluewin.ch>
2021-03-21 11:46:19 +0100

    suppr /juravote-dynam,ique

--- Pages/Resultats/ElectionsCO/Courrendlin/2021-06-13/PresidenceAssemblee.php
+++ Pages/Resultats/ElectionsCO/Courrendlin/2021-06-13/PresidenceAssemblee.php
@@ -36,11 +36,11 @@
 <body>
 <header>
             <div class="navbar-normal">
-                <?php include $_SERVER['DOCUMENT_ROOT']."/Juravote-dynamique/Includes/Navbar/navbar.php"; ?>
+                <?php include $_SERVER['DOCUMENT_ROOT']."/Includes/Navbar/navbar.php"; ?>
             </div>
 
             <div>
-                <?php include $_SERVER['DOCUMENT_ROOT']."/Juravote-dynamique/Includes/Navbar/navbar-mobile.php"; ?>
+                <?php include $_SERVER['DOCUMENT_ROOT']."/Includes/Navbar/navbar-mobile.php"; ?>
             </div>
         </header>
 <main>
@@ -69,7 +69,7 @@
 </main>
 <footer>
         <div class="footer">
-            <?php include $_SERVER['DOCUMENT_ROOT']."/Juravote-dynamique/Includes/Footer/footer.php"; ?>
+            <?php include $_SERVER['DOCUMENT_ROOT']."/Includes/Footer/footer.php"; ?>
         </div>
     </footer>
 </body>

4f4c9fe8c1223cb16003321c144dadfd9be8ea2c
Daniel Renaud <daniel.renaud2000@bluewin.ch>
2021-03-21 11:39:08 +0100

    Update Resultats.php

--- Pages/Resultats/ElectionsCO/Noirmont/2021_03_21/Resultats.php
+++ Pages/Resultats/ElectionsCO/Noirmont/2021_03_21/Resultats.php
@@ -62,9 +62,9 @@
                         </div>
                     </div>
                     <div class="uk-card-body uk-text-left">
-                        <p><b>Objet : </b>Élection à la mairie</p>
-                        <p><b>Participation : </b>##.## %</p>
-                        <p><b>Candidat élu : </b>Gabriel Martinoli - Damien Paratte</p>
+                        <p><b>Objet : </b>Élection à la Mairie</p>
+                        <p><b>Participation : </b>-</p>
+                        <p><b>Candidat élu : </b>-</p>
                         <p><b>Type de l'élection : </b>Élection communale</p>
                         <p><b>Mode d'élection : </b>Élection complémentaire</p>
                         <p><a href="<?php echo $PHPelectionNoirmontDescription20210321 ?>">Lien vers les informations d'élection</a></p>
@@ -75,11 +75,12 @@
                 <div class="uk-card uk-card-default">
                     <div class="uk-card-header">
                         <div class="uk-grid-small uk-flex-column uk-align-left" uk-grid>
-                            <h2>Résultats communaux</h2>
+                            <h2>Résultats</h2>
                         </div>
                     </div>
                     <section style="padding: 2%;">
-                        <iframe title="Election complémentaire à la Mairie du Noirmont" aria-label="Diagramme en barres" id="datawrapper-chart-sJQmm" src="https://datawrapper.dwcdn.net/sJQmm/1/" scrolling="no" frameborder="0" style="width: 0; min-width: 100% !important; border: none;" height="140"></iframe><script type="text/javascript">!function(){"use strict";window.addEventListener("message",(function(a){if(void 0!==a.data["datawrapper-height"])for(var e in a.data["datawrapper-height"]){var t=document.getElementById("datawrapper-chart-"+e)||document.querySelector("iframe[src*='"+e+"']");t&&(t.style.height=a.data["datawrapper-height"][e]+"px")}}))}();</script>
+                        <iframe title="Election complémentaire à la Mairie du Noirmont" aria-label="Diagramme en barres" id="datawrapper-chart-sJQmm" src="https://datawrapper.dwcdn.net/sJQmm/3/" scrolling="no" frameborder="0" style="width: 0; min-width: 100% !important; border: none;" height="141"></iframe><script type="text/javascript">!function(){"use strict";window.addEventListener("message",(function(a){if(void 0!==a.data["datawrapper-height"])for(var e in a.data["datawrapper-height"]){var t=document.getElementById("datawrapper-chart-"+e)||document.querySelector("iframe[src*='"+e+"']");t&&(t.style.height=a.data["datawrapper-height"][e]+"px")}}))}();
+                        </script>
                     </section>
                 </div>
             </div>

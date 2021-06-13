e5606afca3c371bc4070762d4d24b80ad885d7ad
Daniel Renaud <daniel.renaud2000@bluewin.ch>
2021-06-13 09:00:42 +0200

    suppr /juravote-dynamique V2

--- Pages/Resultats/VotationsCH/2030-2021/2021-06-13/vfederales.php
+++ Pages/Resultats/VotationsCH/2030-2021/2021-06-13/vfederales.php
@@ -43,11 +43,11 @@
 <!-- Barre de navigation -->
 <header>
             <div class="navbar-normal">
-                <?php include $_SERVER['DOCUMENT_ROOT']."/Juravote-dynamique/Includes/Navbar/navbar.php"; ?>
+                <?php include $_SERVER['DOCUMENT_ROOT']."/Includes/Navbar/navbar.php"; ?>
             </div>
 
             <div>
-                <?php include $_SERVER['DOCUMENT_ROOT']."/Juravote-dynamique/Includes/Navbar/navbar-mobile.php"; ?>
+                <?php include $_SERVER['DOCUMENT_ROOT']."/Includes/Navbar/navbar-mobile.php"; ?>
             </div>
         </header>
 <main>
@@ -171,7 +171,7 @@
 </main>
 <footer>
         <div class="footer">
-            <?php include $_SERVER['DOCUMENT_ROOT']."/Juravote-dynamique/Includes/Footer/footer.php"; ?>
+            <?php include $_SERVER['DOCUMENT_ROOT']."/Includes/Footer/footer.php"; ?>
         </div>
     </footer>
 </body>
